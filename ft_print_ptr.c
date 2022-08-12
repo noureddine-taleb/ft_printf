9d44c495734c363ad07456173350bef55cfc43c6
Nour-eddine Taleb <contact@noureddine.xyz>
2022-08-12 13:27:22 +0100

    handle null ptr

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -2,20 +2,25 @@
 
 int ft_print_ptr(void *p, struct flags *flags) {
 	flags->hash = 0;
-	char *str = ft_utoa_base((unsigned long)p, "0123456789abcdef");
+	char *str;
 	char *new_str;
 
-	new_str = ft_strjoin("0x", str);
-	free(str);
-	if (flags->space || flags->plus) {
-		if (flags->space) {
-			str = ft_strjoin(" ", new_str);
-		} else {
-			str = ft_strjoin("+", new_str);
+	if (!p) {
+		new_str = "(nil)";
+	} else {
+		str = ft_utoa_base((unsigned long)p, "0123456789abcdef");
+		new_str = ft_strjoin("0x", str);
+		free(str);
+		if (flags->space || flags->plus) {
+			if (flags->space) {
+				str = ft_strjoin(" ", new_str);
+			} else {
+				str = ft_strjoin("+", new_str);
+			}
+			free(new_str);
+			new_str = str;
 		}
-		free(new_str);
-		new_str = str;
 	}
 
-	return ft_print_padded_str(new_str, flags, 1);
+	return ft_print_padded_str(new_str, flags, p != NULL);
 }
