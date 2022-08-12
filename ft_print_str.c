de2d17c2fffe505edf38610fba1912f4750baed1
Noureddine Taleb <ntaleb@1337.ma>
2022-08-12 12:20:50 +0100

    handle null string

--- ft_print_str.c
+++ ft_print_str.c
@@ -2,5 +2,7 @@
 
 int ft_print_str(char *str, struct flags *flags) {
 	flags->hash = flags->plus = flags->space = flags->zero = 0;
+	if (!str)
+		str = "(null)";
 	return ft_print_padded_str(str, flags, 0);
 }
